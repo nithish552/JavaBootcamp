f31880737d6f706aa74e493a7c6f1dc9e4a13eac
Nithishbabu <nith21ad041@rmkcet.ac.in>
2025-09-09 18:11:12 +0530

    LightBulbTest completed

--- src/LightBulbTest.java
+++ src/LightBulbTest.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         int val=in.nextInt();
         if(val%2==0){
             System.out.println("ON");
